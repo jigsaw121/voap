c4e0b8254d45cc62ae3c1a7b18a4b6f27b5aabb3
jigsaw121 <tt.tuomas@gmail.com>
2013-12-27 20:57:28 +0200

    about time it started working again

--- refactor.cpp
+++ refactor.cpp
@@ -267,6 +267,34 @@ Weapon* Weapon::register_fac() {
 	return new Weapon();
 }
 
+class Explosive: public Interactive() {
+	public:
+		explicit Explosive(): Interactive() {}		
+} 
+
+class Boom: public Interactive() {
+	public:
+		explicit Boom(): Interactive() {}
+		virtual void act();
+}
+void Boom::act() {
+	if (collide(/*any type except for this one*/)) {
+		// some types blow up when damaged, creating more booms on the next frames
+		// could break walls too
+		target.damage();
+	}
+}
+
+class WallAmbassador: public Interactive() {
+	// haha dese names man
+	// basically an interface to handle wall collisions
+	// -> collide with this instead of the image layers directly
+	// an inherited version could destroy the walls on collision too
+	// quadtrees and shiz whenever needed, even a couple of huge layers run smoothly right now
+	public:
+		explicit WallAmbassador(): Interactive() {}
+}
+
 template <class T>
 void State::remove(T* obj) {
 	erasebuffer.push_back(T);
